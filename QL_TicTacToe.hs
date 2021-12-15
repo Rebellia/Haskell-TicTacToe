13b36a3d1deb5f24d1917f94907f55e7f1585e17
Rebellia <sleepy.wrath@gmail.com>
2021-12-15 16:08:43 -0700

    debugged some error type messages

--- QL_TicTacToe.hs
+++ QL_TicTacToe.hs
@@ -8,7 +8,7 @@ Some of my function declarations, [functionName] :: [input] -> [output], were ca
 --------------   Print the Board ------------------
 --- To print the current board ---
 
---printActualBoard [String] -> IO()
+printActualBoard :: [(String, String)] -> IO()
 printActualBoard myBoard = do
    putStrLn("\n Here is the current board")
    putStrLn ((snd (myBoard !! 0)) ++ (snd (myBoard !! 1)) ++ (snd (myBoard !! 2)))
@@ -18,7 +18,7 @@ printActualBoard myBoard = do
 
 --- To print the numpad illustration ---
 
---printDemoBoard [String] -> IO()
+printDemoBoard :: IO()
 printDemoBoard = do
     let myBoard = ["7", "8", "9", "4", "5", "6", "1", "2", "3"]
     putStrLn "\nThe game board is laid out like a number pad:"
@@ -30,7 +30,7 @@ printDemoBoard = do
 ----------   Play the Game --------------------------
 
 
---playGame :: -> IO()
+playGame :: [Char] -> IO()
 playGame currentPlayer = do
    -- Board was initially a global variable, but that wasn't updating the way it should have been
    let board = [("7", "-"), ("8", "-"), ("9", "-"), ("4", "-"), ("5", "-"), ("6", "-"), ("1", "-"), ("2", "-"), ("3", "-")]
@@ -38,79 +38,108 @@ playGame currentPlayer = do
    takeTurn board currentPlayer
 
 
---takeTurn :: [String] -> String -> IO()
+takeTurn :: [([Char], [Char])] -> String -> IO()
 takeTurn currentBoard currentPlayer = do
   --Initially board was in here, but I realized that it would constantly be overwritten if I did that
   putStrLn "\n This is the currentBoard: "
   printActualBoard currentBoard
   
-  -- Check if the board is full
   if isTie currentBoard
   then
       putStrLn "It's a tie!"
+  else if hasWon currentBoard currentPlayer
+      then do putStrLn ("\nPlayer " ++ currentPlayer ++ " won!")
+  else if hasWon currentBoard (changePlayer currentPlayer)
+      then do putStrLn ("\nPlayer " ++ (changePlayer currentPlayer) ++ " won!")
   else do
-      putStrLn "The board is full so there is a tie!"
+      putStrLn "Please pick a number, 1-9"
+      chosenNumber <- getLine
+
+      if isValid chosenNumber currentBoard
+      then do
+        let updatedBoard = markSpace currentBoard chosenNumber currentPlayer
+        putStrLn ("\n Player " ++ currentPlayer ++ "has marked a space")
+        takeTurn updatedBoard (changePlayer currentPlayer)
+      else
+        putStrLn "Invlaid input. Please try again"
+        takeTurn currentBoard currentPlayer
 
   
 
+changePlayer :: String -> String
+changePlayer currentPlayer = 
+    if currentPlayer == "x"
+    then "o"
+    else "x"
+
 
 --markSpace :: [(String, String)] -> String -> String -> [(String, String)]
-markSpace myBoard currentPlayer chosenSpace = do
-    map(\ x -> if x == (number, ' ') then (number, 'x') else x) boardList
+{-
+markSpace myBoard chosenSpace currentPlayer = do
+    map(\ x -> if x == (chosenSpace, ' ') then (chosenSpace, 'x') else x) myBoard -}
+markSpace myBoard chosenSpace currentPlayer = 
+    map(\ x -> if x == (chosenSpace, ' ') then (chosenSpace, 'x') else x) myBoard
 
 ------------ Gameplay Helper Functions -------------------------
 --isValid :: (String, String) -> Bool
-isValid space myBoard = elem (number, "-") simpleList
+isValid space myBoard = elem (space, "-") myBoard
 
 --anyEmpty:: [(String, String)] -> Bool
 anyEmpty myBoard = do
-    let count = 0 && count <= 9
-    if elem (count, "-") myBoard
+    let count = 1
+    if elem (count, "-") myBoard && count <= 9
     then True
     else do
        anyEmpty elem (count+1, "-")
 
-checkSame elem1 elem2 elem3 =
+
+--- Checking Winner ---
+checkSame :: [(String, String)] -> String -> String -> String -> Bool
+checkSame currentPlayer elem1 elem2 elem3 =
     -- If any are empty, no winner from this combo
     if elem1 == "-" || elem2 == "-" || elem3 == "-"
     then False
     else
-        if (==) elem1 elem2 && (==) elem1 elem3
+        if (==) elem1 elem2 && (==) elem1 elem3 && (==) elem1 currentPlayer
         then True
         else False
 
-grabChars myBoard index1 index2 index3 =
-    let elem1 = snd (myList !! index1)
-        elem2 = snd (myList !! index2)
-        elem3 = snd (myList !! index3)
-    in checkSame elem1 elem2 elem3
 
 
---isTie :: [String] -> Bool
+grabChars currentPlayer myBoard index1 index2 index3 =
+    let elem1 = snd (myBoard !! index1)
+        elem2 = snd (myBoard !! index2)
+        elem3 = snd (myBoard !! index3)
+    in checkSame currentPlayer elem1 elem2 elem3
+
+
+isTie :: [(String, String)] -> Bool
 isTie myBoard =
-    if (anyEmpty myBoard == False) && (hasWon myBoard "x" == False) && (hasWon myBoard "y" == False)
+    if (anyEmpty myBoard == False) && (hasWon myBoard "x" == False) && (hasWon myBoard "o" == False)
     then True
     else False
 
+
+hasWon :: [(String, String)] -> String -> Bool
 hasWon myBoard currentPlayer =
    -- horizontals
-   if grabChars myList 0 1 2
+   if grabChars currentPlayer myBoard 0 1 2
       then True
-   else if grabChars myList 3 4 5
+   else if grabChars currentPlayer myBoard 3 4 5
       then True
-   else if grabChars myList 6 7 8
+   else if grabChars currentPlayer myBoard 6 7 8
       then True
    -- verticals
-   else if grabChars myList 1 4 7
+   else if grabChars currentPlayer myBoard 1 4 7
       then True
-   else if grabChars myList 2 5 8
+   else if grabChars currentPlayer myBoard 2 5 8
       then True
-   else if grabChars myList 3 6 9
+   else if grabChars currentPlayer myBoard 3 6 9
       then True
    -- diagonals
-   else if grabChars myList 1 5 9
+   else if grabChars currentPlayer myBoard 1 5 9
       then True
-   else if grabChars myList 3 5 7
+   else if grabChars currentPlayer myBoard 3 5 7
       then True
    else False
    
